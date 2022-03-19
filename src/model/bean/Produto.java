f82af1a189e16cc9cf5ef910b94cea5e62b1096e
estev <estev@Estevan-Moreira>
2022-03-18 20:27:58 -0400

    BEANS DOS OBJETOS

---
+++ src/model/bean/Produto.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.bean;
+
+/**
+ *
+ * @author estev
+ */
+public class Produto {
+    
+    private int id_produto, qtd;
+    private float precoCompra, precoVenda;
+    private String nome;
+
+    /**
+     * @return the id_produto
+     */
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    /**
+     * @param id_produto the id_produto to set
+     */
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    /**
+     * @return the qtd
+     */
+    public int getQtd() {
+        return qtd;
+    }
+
+    /**
+     * @param qtd the qtd to set
+     */
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    /**
+     * @return the precoCompra
+     */
+    public float getPrecoCompra() {
+        return precoCompra;
+    }
+
+    /**
+     * @param precoCompra the precoCompra to set
+     */
+    public void setPrecoCompra(float precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    /**
+     * @return the precoVenda
+     */
+    public float getPrecoVenda() {
+        return precoVenda;
+    }
+
+    /**
+     * @param precoVenda the precoVenda to set
+     */
+    public void setPrecoVenda(float precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+  
+    
+}
